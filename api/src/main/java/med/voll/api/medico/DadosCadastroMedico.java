bedc9c3bbe2669814607a82374212e2cee7bfe88
luiz <luizeduardotsdev@gmail.com>
2025-10-15 12:43:30 -0300

    feat: adiciona a camada de medico

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,4 +1,6 @@
 package med.voll.api.medico;
 
-public record DadosCadastroMedico() {
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
