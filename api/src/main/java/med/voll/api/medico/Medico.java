0589391681164c8afa073ee2ce0a008393965f4c
luiz <luizeduardotsdev@gmail.com>
2025-10-17 18:22:20 -0300

    feat: cria a entidade medico e extend ao repository

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -1,4 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Data
+@EqualsAndHashCode(of = "id")
 public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco enderco;
+
 }
