1bf348f935272a0b8de6a798f610e6ed00aaa9d1
luiz <luizeduardotsdev@gmail.com>
2025-10-17 18:43:03 -0300

    feat: cria a entidade paciente e implementa ao jpa repository

---
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,36 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
