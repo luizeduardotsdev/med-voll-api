435ef0af38af52ab89845081062887d276948869
luiz <luizeduardotsdev@gmail.com>
2025-10-17 19:20:53 -0300

    adiciona a migration V1 do db

--- api/src/main/resources/db/migration/V1__create-table-medicos.sql
+++ api/src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -1,16 +1,18 @@
-crete table medicos(
-      id biginit not null auto_incremente,
-      nome varchar(100) not null,
-      email varchar(100) not null unique,
-      crm varchar(6) not null unique,
-      especialidade varchar(100) not null,
-      logradouro varchar (100) not null,
-      bairro varchar(100) not bull,
-      cep varchar(9) not null,
-      complemento varchar(100),
-      numero varchar(20),
-      uf char(2) not null,
-      cidade varchar(100) not null,
+CREATE TABLE medicos (
 
-      primary key(id)
-)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    crm VARCHAR(6) NOT NULL UNIQUE,
+    especialidade VARCHAR(100) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    complemento VARCHAR(100),
+    numero VARCHAR(20),
+    uf CHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id)
+
+);
