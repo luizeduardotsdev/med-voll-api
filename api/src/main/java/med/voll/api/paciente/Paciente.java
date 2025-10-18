98de453d33ebafd9d3860d9afc31c89a2663fe12
luiz <luizeduardotsdev@gmail.com>
2025-10-18 12:36:59 -0300

    feat: cria metodo de atualizacao de paciente na entidade

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosAtualizadoMedico;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
@@ -32,5 +33,17 @@ public class Paciente {
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualuzarInformacoes(DadosAtualizadoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformarcoes(dados.endereco());
+        }
     }
 }
