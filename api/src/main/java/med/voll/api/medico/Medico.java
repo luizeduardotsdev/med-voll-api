b412d9de3fa1edf4f8db9916b4af3dbe30f867e8
luiz <luizeduardotsdev@gmail.com>
2025-10-18 12:29:37 -0300

    feat: implementa metodos de atualizacao nas entidades

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -1,11 +1,13 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
@@ -37,5 +39,17 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.enderco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizadoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.enderco.atualizarInformarcoes(dados.endereco());
+        }
     }
 }
