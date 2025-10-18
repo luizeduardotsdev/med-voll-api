9d890ca8756a3ccc9a1b99ec32d21ecc53e1d8eb
luiz <luizeduardotsdev@gmail.com>
2025-10-18 11:48:47 -0300

    feat: adiciona @PageableDefault com size = 10 e sort = nome

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -8,6 +8,7 @@ import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +26,7 @@ public class PacienteController {
     }
 
     @GetMapping("/listar")
-    public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
